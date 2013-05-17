29132ba96f20f197fefec044f029872b2131065a
JAkzam <JAkzam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-17 18:28:17 +0000

    This is the first commit to the repository
    git-svn-id: https://plugins.svn.wordpress.org/post-list-featured-image/trunk@714491 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ posts-list-featured-image.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Plugin Name: Posts List Featured Image
+Plugin URI: http://jaggededgemedia.com
+Description: Adds a featured image column in admin Posts/Pages list.
+Version: 0.1.0
+Author: Jagged Edge Media
+Author URI: http://jaggededgemedia.com
+*//*This file is part of Post List Featured Image.Post List Featured Image is free software: you can redistribute it and/or modifyit under the terms of the GNU General Public License as published bythe Free Software Foundation, either version 3 of the License, or(at your option) any later version.Post List Featured Image is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with Foobar.  If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.*/
+if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
+	exit('This page cannot be called directly.');
+}
+
+class PostsListFeaturedImage {
+
+	public function PostsListFeaturedImage() {
+		$this->__construct();
+	}
+
+	public function __construct() {
+		if (function_exists('add_theme_support')) {
+			add_image_size('admin-thumbs', 100, 100);
+			add_filter('manage_posts_columns', array(&$this, 'posts_columns'), 5);
+			add_filter('manage_pages_columns', array(&$this, 'posts_columns'), 5);
+			add_action('manage_posts_custom_column', array(&$this, 'posts_custom_columns'), 5, 2);
+			add_action('manage_pages_custom_column', array(&$this, 'posts_custom_columns'), 5, 2);
+		}
+	}
+
+	public function posts_columns($defaults) {
+		$defaults['featured_image'] = __('Featured Image');
+
+		return $defaults;
+	}
+
+	public function posts_custom_columns($column_name, $id) {
+		if ($column_name === 'featured_image') {
+			echo the_post_thumbnail('admin-thumbs');
+		}
+	}
+
+}
+
+global $plfi;
+$plfi = new PostsListFeaturedImage();
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Name ===
+Contributors: JAkzam, PCGrejaold
+Donate link: http://jaggededgemedia.com/donate/
+Tags: featured, image, posts, pages
+Requires at least: 3.5.1
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+A plugin that adds the "Featured Image" column in admin posts and pages list.
+
+== Description ==
+
+A plugin that adds the "Featured Image" column in admin posts and pages list. It lets the wordpress site owners see
+which posts or pages have its featured image set.
+
+== Installation ==
+
+1. Upload posts-list-featured-image.zip via "wp-admin/plugin-install.php?tab=upload" or if using FTP, unzip
+posts-list-featured-image.zip and upload `/posts-list-featured-image/` directory to the `/wp-content/plugins/`
+directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress (under plugin name:
+**"Posts List Featured Image"**).
+3. Find more Installation options at http://jaggededgemedia.com/plugins/post-list-featured-image/
+
+== Frequently Asked Questions ==
+
+= I have some questions. How do I contact you? =
+
+Send them to support@jaggededgemedia.com
+
+== Screenshots ==
+
+1. Screenshot shows the "Featured Image" column added in Posts list.
+2. Screenshot shows the "Featured Image" column added in Pages list.
+
+== Changelog ==
+
+= 0.1.0 =
+* Adds the "Featured Image" column in the admin posts and pages list.== Changelog ==
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+We aim to stay current with the latest release of WordPress. Not upgrading may cause the feature to break if you upgrade your WordPress install.
